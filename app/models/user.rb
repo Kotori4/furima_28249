35b4a174b4a9b8c2231822875d640496408b195c
Kotori4 <68325394+Kotori4@users.noreply.github.com>
2020-08-06 11:40:03 +0900

    正規表現を変数に代入

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,13 +4,18 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+
+    alpha_num = /\A[a-z]+[0-9]+\z/i
+    js = /\A[ぁ-んァ-ン一-龥]/
+    js_kana = /\A[ァ-ヶー－]+\z/
+
     with_options presence: true do
     validates :nickname
-    validates :password, format: { with:  /\A[a-z ]+[0-9]+\z/i, message: "is invalid. Input half-width characters and number."}
-    validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width characters."}
-    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input full-width characters."}
-    validates :last_name_reading, format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters."}
-    validates :first_name_reading, format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters."}
+    validates :password, format: { with: alpha_num , message: "is invalid. Input half-width characters and number."}
+    validates :last_name, format: { with: js, message: "is invalid. Input full-width characters."}
+    validates :first_name, format: { with: js, message: "is invalid. Input full-width characters."}
+    validates :last_name_reading, format: { with: js_kana, message: "is invalid. Input full-width katakana characters."}
+    validates :first_name_reading, format: { with: js_kana, message: "is invalid. Input full-width katakana characters."}
     validates :birthday
   end
 end
