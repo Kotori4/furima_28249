44e5b7133101064192b099e044f7938821a2af3c
Kotori4 <68325394+Kotori4@users.noreply.github.com>
2020-08-06 19:25:12 +0900

    アイテム関連のアソシエーション実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,4 +3,10 @@ class ItemsController < ApplicationController
   def index 
        
   end
+
+  private
+
+  def item_params
+    params.require(:user).permit(:image).merge(user_id: current_user.id)
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+    has_many :items
+    has_many :purchases
 
     alpha_num = /\A[a-z]+[0-9]+\z/i
     js = /\A[ぁ-んァ-ン一-龥]/
