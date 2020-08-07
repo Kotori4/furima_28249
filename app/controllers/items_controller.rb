269ee9d60123ad2529001d9f7635404b73715012
Kotori4 <68325394+Kotori4@users.noreply.github.com>
2020-08-07 12:28:01 +0900

    ActvieHashをビューファイルに実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -9,12 +9,12 @@ class ItemsController < ApplicationController
   end
 
   def create
-    @item = Item.new(item_params)
-    if @item.save
-      redirect_to root_path
-    else
-      render 'new'
-    end
+    # @item = Item.new(item_params)
+    # if @item.save
+    #   redirect_to root_path
+    # else
+    #   render 'new'
+    # end
   end
 
   def edit
@@ -25,7 +25,7 @@ class ItemsController < ApplicationController
 
   private
 
-  def item_params
-    params.require(:user).permit(:image).merge(user_id: current_user.id)
-  end
+  # def item_params
+  #   params.require(:user).permit(:image, :item_name, :item_detail, :item_category, :item_condition, :covered_shipping_by, :ship_from_location, :days_til_ship, :price).merge(user_id: current_user.id, address_id: current_user.address_id)
+  # end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,23 @@ class Item < ApplicationRecord
   belongs_to :user
   has_one    :purchase
   has_one_attached :image
+
+  with_options presence: true do
+    validates :item_name, length: {maximum: 40}
+    validates :item_detail, length: {maximum: 1000}
+    validates :item_category, numericality: { other_than: 1 }
+    validates :item_condition, numericality: { other_than: 1 }
+    validates :covered_shipping_by, numericality: { other_than: 1 }
+    validates :ship_from_location, numericality: { other_than: 1 }
+    validates :days_til_ship, numericality: { other_than: 1 }
+    validates :price, length: { minimum: 300, maximum: 9999999}
+  end
+
+  extend ActiveHash::Associations::ActiveRecordExtensions
+    belongs_to_active_hash :item_category
+    belongs_to_active_hash :item_condition
+    belongs_to_active_hash :covered_shipping_by
+    belongs_to_active_hash :ship_from_location
+    belongs_to_active_hash :days_til_ship
 end
+
