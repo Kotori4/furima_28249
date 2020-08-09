164d620da8f3bf6e30397c83447b267260074f1a
Kotori4 <68325394+Kotori4@users.noreply.github.com>
2020-08-09 18:29:13 +0900

    faker使用の削除

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,13 +1,13 @@
 FactoryBot.define do
   factory :item do
     item_name                       {"テスト用のアイテム名"}
-    item_detail                     {Faker::Lorem.sentence}
-    item_category_id                {Faker::Number.between(from: 1, to: 10)}
-    item_condition_id               {Faker::Number.between(from: 1, to: 6)}
-    covered_shipping_by_id          {Faker::Number.between(from: 1, to: 2)}
-    ship_from_location_id           {Faker::Number.between(from: 1, to: 47)}
-    days_til_ship_id                {Faker::Number.between(from: 1, to: 3)}
-    price                           {Faker::Number.between(from: 300, to: 1000000)}
+    item_detail                     {"テスト用のアイテム詳細"}
+    item_category_id                {1}
+    item_condition_id               {2}
+    covered_shipping_by_id          {2}
+    ship_from_location_id           {15}
+    days_til_ship_id                {3}
+    price                           {5000}
     user
   end
 end
