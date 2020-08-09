ee983a2a01cf64e8933689af40881d3c92fe8324
Kotori4 <68325394+Kotori4@users.noreply.github.com>
2020-08-09 16:44:49 +0900

    テストコードの実装

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,6 +4,7 @@ class Item < ApplicationRecord
   has_one_attached :image
 
   with_options presence: true do
+    validates :image
     validates :item_name, length: {maximum: 40}
     validates :item_detail, length: {maximum: 1000}
     validates :item_category_id
---
+++ spec/factories/items.rb
@@ -0,0 +1,14 @@
+FactoryBot.define do
+  factory :item do
+    item_name                       {Faker::Book.title}
+    item_detail                     {Faker::Lorem.sentence}
+    item_category_id                {Faker::Number.between(from: 1, to: 10)}
+    item_condition_id               {Faker::Number.between(from: 1, to: 6)}
+    covered_shipping_by_id          {Faker::Number.between(from: 1, to: 2)}
+    ship_from_location_id           {Faker::Number.between(from: 1, to: 47)}
+    days_til_ship_id                {Faker::Number.between(from: 1, to: 3)}
+    price                           {Faker::Number.between(from: 300, to: 1000000)}
+    user
+  end
+end
+
